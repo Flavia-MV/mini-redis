627083e13b11df74e2f9a0261de00ab0baaa0649
Flavia-Mihaela Vadeanu <flaviavadeanu@gmail.com>
2026-09-07 16:24:59 +0300

    implemented epoll-based event loop for multiple concurrent clients

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/epoll.h>
 #include "server.h"
 #include "parser.h"
 #include "hashtable.h"
@@ -13,67 +14,98 @@ int main(void) {
     int server_fd = start_server(6379);
     printf("Server listening on port 6379...\n");
 
-    struct sockaddr_in client_address;
-    socklen_t client_len = sizeof(client_address);
-    int client_fd = accept(server_fd, (struct sockaddr *)&client_address, &client_len);
-    if (client_fd < 0) {
-        perror("accept failed");
+   int epoll_fd = epoll_create1(0);
+    if (epoll_fd < 0) {
+        perror("epoll_create1 failed");
         exit(EXIT_FAILURE);
     }
-    printf("Client connected\n");
+    struct epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = server_fd;
 
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event) < 0) {
+        perror("epoll_ctl failed");
+        exit(EXIT_FAILURE);
+    }
+
+    struct epoll_event events[64];
     char buffer[1024];
     while (1) {
-        ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
-        if (bytes_read <= 0) {
-           printf("Client disconnected\n");
-           break;
+        int num_events = epoll_wait(epoll_fd, events, 64, -1);
+        if (num_events < 0) {
+            perror("epoll_wait failed");
+            exit(EXIT_FAILURE);
         }
-        buffer[bytes_read] = '\0';
-        buffer[strcspn(buffer, "\r\n")] = '\0';
 
-        Command cmd = parse_command(buffer);
-        switch (cmd.type)
-        {
-        case CMD_SET: {
-            ht_set(ht, cmd.key, cmd.value);
-            write(client_fd, "OK\n", 3);
-            break;
-        }
-        case CMD_GET: {
-            const char *value = ht_get(ht, cmd.key);
-            char response[512];
-            if (value) {
-                snprintf(response, sizeof(response), "%s\n", value);
+        for (int i = 0; i < num_events; i++) {
+            int current_fd = events[i].data.fd;
+
+            if (current_fd == server_fd) {
+
+                int client_fd = accept(server_fd, NULL, NULL);
+                if (client_fd < 0) {
+                    perror("accept failed");
+                    continue;
+                }
+                printf("Client connected: fd=%d\n", client_fd);
+
+                struct epoll_event client_event;
+                client_event.events = EPOLLIN;
+                client_event.data.fd = client_fd;
+                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &client_event);
             }
             else {
-                snprintf(response, sizeof(response), "(nil)\n");
+                ssize_t bytes_read = read(current_fd, buffer, sizeof(buffer) - 1);
+                if (bytes_read <= 0) {
+                    printf("Client disconnected: fd=%d\n", current_fd);
+                    close(current_fd);
+                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, current_fd, NULL);
+                    continue;
+                }
+                buffer[bytes_read] = '\0';
+                buffer[strcspn(buffer, "\r\n")] = '\0';
+
+                Command cmd = parse_command(buffer);
+                switch (cmd.type)
+                {
+                case CMD_SET: {
+                    ht_set(ht, cmd.key, cmd.value);
+                    write(current_fd, "OK\n", 3);
+                    break;
+                }
+                case CMD_GET: {
+                    const char *value = ht_get(ht, cmd.key);
+                    char response[512];
+                    if (value) {
+                        snprintf(response, sizeof(response), "%s\n", value);
+                    }
+                    else {
+                        snprintf(response, sizeof(response), "(nil)\n");
+                    }
+                    write(current_fd, response, strlen(response));
+                    break;
+                }
+                case CMD_DEL: {
+                    int deleted = ht_delete(ht, cmd.key);
+                    if (deleted) 
+                        write(current_fd, "OK\n", 3);
+                    else
+                        write(current_fd, "(nil)\n", 6);
+                    break;
+                }
+                case CMD_UNKNOWN: {
+                    const char *msg = "ERROR unknown command\n";
+                    write(current_fd, msg, strlen(msg));
+                    break;
+                }
+                default:
+                    break;
+                }
             }
-            write(client_fd, response, strlen(response));
-            break;
         }
-        case CMD_DEL: {
-            int deleted = ht_delete(ht, cmd.key);
-            if (deleted) 
-                write(client_fd, "OK\n", 3);
-            else
-                write(client_fd, "(nil)\n", 6);
-            break;
-        }
-        case CMD_UNKNOWN: {
-            const char *msg = "ERROR unknown command\n";
-            write(client_fd, msg, strlen(msg));
-            break;
-        }
-        default:
-            break;
-        }
+       
     }
    
-
-
-    close(client_fd);
-    close(server_fd);
     ht_destroy(ht);
     return 0;
 }
